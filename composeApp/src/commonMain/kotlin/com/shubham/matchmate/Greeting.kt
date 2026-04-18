db7fa4af4f130b8d2e709b44ccedd877a38987e9
Shubham Singh <shubham.singh9@phonepe.com>
2026-04-18 16:08:36 +0530

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/shubham/matchmate/Greeting.kt
@@ -0,0 +1,9 @@
+package com.shubham.matchmate
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/com/shubham/matchmate/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.shubham.matchmate
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
